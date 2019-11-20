458cd89b5520325c139033364352186302b395a5
ke-chan <32466454+ke-chan@users.noreply.github.com>
2019-11-20 14:14:46 -0600

    make sentence lower case and remove punctuation

--- text_to_emotion.py
+++ text_to_emotion.py
@@ -1,23 +1,38 @@
 import pickle
+import requests
+import string
+from bs4 import BeautifulSoup
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize 
+from nltk.tokenize import RegexpTokenizer
 
 #open the word_emotion_dict from the pickle
 with open('word_emotion_dict.pickle', 'rb') as handle:
     word_emotion_dict = pickle.load(handle)
 
-def get_words(filename):
+def get_words_from_text(filename):
     #read in a text file and return a list of words from the text file with stopwords removed
     with open(filename, 'r') as file:
         data = file.read().replace('\n', ' ')
     stop_words = set(stopwords.words('english')) 
-    word_tokens = word_tokenize(data) 
-    #print(data)
+    data = data.lower()
+    tokenizer = RegexpTokenizer(r'\w+')
+    word_tokens = tokenizer.tokenize(data)
     filtered_sentence = [w for w in word_tokens if not w in stop_words] 
     #print(filtered_sentence)
     return filtered_sentence
+#get_words_from_text("test.txt")
 
-#get_words("test.txt")
-    
+def get_words_from_wikiurl(url):
+    res = requests.get(url)
+    html_page = res.text
+    soup = BeautifulSoup(html_page, "html.parser")
+    #print(soup.prettify().encode("utf-8"))
+    print()
+
+
+#get_words_from_wikiurl("https://starwars.fandom.com/wiki/Luke_Skywalker")
+print(get_words_from_text("test.txt"))
+
 
 
