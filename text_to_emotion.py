ea9307ca7d641a33aa8247bbb924f089e6df2589
ke-chan <32466454+ke-chan@users.noreply.github.com>
2019-11-20 14:42:31 -0600

    add text scraping from url

--- text_to_emotion.py
+++ text_to_emotion.py
@@ -1,6 +1,5 @@
 import pickle
 import requests
-import string
 from bs4 import BeautifulSoup
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize 
@@ -27,12 +26,19 @@ def get_words_from_wikiurl(url):
     res = requests.get(url)
     html_page = res.text
     soup = BeautifulSoup(html_page, "html.parser")
-    #print(soup.prettify().encode("utf-8"))
-    print()
-
+    paragraphs = list(str(soup.find_all("p")))
+    paragraphs = "".join(paragraphs)
+    soup = BeautifulSoup(paragraphs, "html.parser")
+    data = soup.get_text()
+    stop_words = set(stopwords.words('english')) 
+    data = data.lower()
+    tokenizer = RegexpTokenizer(r'\w+')
+    word_tokens = tokenizer.tokenize(data)
+    filtered_sentence = [w for w in word_tokens if not w in stop_words] 
+    return filtered_sentence
 
 #get_words_from_wikiurl("https://starwars.fandom.com/wiki/Luke_Skywalker")
-print(get_words_from_text("test.txt"))
+#print(get_words_from_text("test.txt"))
 
 
 
